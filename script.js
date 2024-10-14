144284222c907507c6239dec6bd021564aab4908
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-10-14 21:33:28 +0300

    gsap

---
+++ script.js
@@ -0,0 +1,31 @@
+const navbar = document.querySelector('#navbar');
+const close = document.querySelector('.close');
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click',() => {
+    navbar.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    navbar.classList.remove('active');
+});
+
+// website animation
+
+gsap.from('.home-text', {
+    duration: 1,
+    y:200,
+    opacity:0
+});
+
+gsap.from('.contact-us',{
+    opacity: 0,
+    y: 300,
+    duration: 1,
+    scrollTrigger: {
+        trigger:'.contact-us',
+        start:'top 80%',
+        end:'top50%',
+        toggleActions:'play none none none'
+    }
+});
